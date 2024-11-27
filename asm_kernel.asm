9b52beb16328b9dbbba16f0ff7382c91e615d0cc
MeerKT <166136002+MeerKT@users.noreply.github.com>
2024-11-27 18:52:10 +0800

    debugged

--- asm_kernel.asm
+++ asm_kernel.asm
@@ -1,36 +1,39 @@
+
 section .text
 bits 64
 default rel
-global daxpy
+
+global daxpy_asm
+
    
-    daxpy:
+    daxpy_asm:
         push rbp ;stack to frame
         mov rbp, rsp
         add rbp, 16
         
         mov r15, [rbp+32] ;store Z
        
-        xor r10, r10
+        xor rbx, rbx
        
     L1:
-        cmp r10, rcx
+        cmp rbx, rcx
         jae end_loop
         
         ;Calculates A*X[i]
         xorpd xmm7, xmm7
         
-        movsd xmm7, [r8+r10*8]
+        movsd xmm7, [r8+rbx*8]
         mulsd xmm7, xmm1
         
         ;Calculate Y[i] + product
         xorpd xmm8, xmm8
-        movsd xmm8, [r9+r10*8]
+        movsd xmm8, [r9+rbx*8]
         addsd xmm7, xmm8
         
        ;store Z[i]
-        movsd [r15+r10*8], xmm7
+        movsd [r15+rbx*8], xmm7
         
-        inc r10 ;increment cnt
+        inc rbx ;increment cnt
         jmp L1
         
     end_loop:
